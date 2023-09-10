41647d375cd3563ebad20edfd96cd11083da2fb3
Adam Zieman <zieman.adam1996@gmail.com>
2023-09-10 00:28:10 -0500

    Add files via upload

---
+++ butterfly.py
@@ -0,0 +1,46 @@
+# butterfly.py
+# This program draws a picture of a butterfly using ASCII Art.
+# A user inputs the number of days until Spring begins, followed by a print statement.
+# Based on original ASCII art image by Joan G. Stark
+
+# Course: CSCI 130 (Introduction To Programming)
+# Assignment: 1
+
+# Author: Adam Zieman
+# Date: February 11, 2021
+
+def main():
+
+    # The following lines asks for days until Spring
+    # and draws a butterfly image
+
+    # Ask the user for # of days until Spring
+    days = int(input("Type number of days until Spring: "))
+    print("Only", days, "days until Spring!")
+    print()
+
+    # Draw the butterfly using print statements
+    print("   _                           _")
+    print("  / `._                     _.' \\")
+    print(" ( @ : `.                 .' : @ )")
+    print("  \  `.  `.  ._     _.  .'  .'  /")
+    print("   \;' `.  `.  \   /  .'  .' `;/")
+    print("    \`. o`.  \  \_/  /  .' o.'/")
+    print("     ) :-._`. \ (:) / .'_.-: (")
+    print("     (`.....,`.\/:\/.',.....')")
+    print("      >------._|:::|_.------<")
+    print("     / .'._>_.-|:::|-._<_.'. \\")
+    print("     |* _.-'_.-^|:|^-._`-._ *|")
+    print("     |`'   ;_.-'|:|`-._;   `'|")
+    print("     \".o_.-' ;.\"|:|\".; `-._o.\"  ")
+    print("       \".__.\"   \\:/   \".__.\"")
+    print("                 v")
+
+
+
+    print()
+    print("Program: Butterfly.py");
+    print("Written by: Adam Zieman");
+
+main() # Calls the main() function
+    
